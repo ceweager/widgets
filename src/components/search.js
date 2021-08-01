5aa1c00a1abf3ced873717f1bcd689f497418e2e
Charlotte Weager <senwyn@googlemail.com>
2021-08-01 17:53:56 +0100

    added fetch api request to wikipedia site

--- src/components/search.js
+++ src/components/search.js
@@ -2,6 +2,33 @@ import React, { useState, useEffect } from 'react';
 
 const Search = () => {
   const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const search = async () => {
+      const data = await fetch(`https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srsearch=${term}`)
+        .then(response => response.json())
+      setResults(data.query.search);
+    };
+    if (term) {
+      search();
+    }
+  }, [term]); // option to have empty array, array with term inside, or no argument
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="content">
+          <div className="header">
+            {result.title}
+          </div>
+          <div className="body">
+            <span dangerouslySetInnerHTML={{ __html: result.snippet }} />
+          </div>
+        </div>
+      </div>
+    );
+  });
   return (
     <div>
       <div className="ui form">
@@ -13,6 +40,9 @@ const Search = () => {
             className="input"
           />
         </div>
+      </div>
+      <div className="ui celled list">
+        {renderedResults}
       </div>
     </div>
   )
